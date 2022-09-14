de44bde19c07ed6df9c045724bb81fd43ecbac28
João Marcos <jmarcossferreira@hotmail.com>
2022-09-14 14:23:30 -0300

    Create external PDF route

--- src/pages/External/index.tsx
+++ src/pages/External/index.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { PDFViewer } from "components/PDFViewer";
+import React, { useEffect, useState } from "react";
 
 export const External = () => {
-  return <div>External</div>;
+  const url = "http://localhost:3000/pdf";
+
+  return (
+    <section>
+      <h1 className="text-lg text-center pb-4">PDF loaded via API</h1>
+      <PDFViewer file={url} />
+    </section>
+  );
 };
--- src/pages/Internal/index.tsx
+++ src/pages/Internal/index.tsx
@@ -5,7 +5,7 @@ import lorem from "assets/lorem.pdf";
 export const Internal = () => {
   return (
     <section>
-      <h1 className="text-lg text-center pb-4">PDF loaded by assets</h1>
+      <h1 className="text-lg text-center pb-4">PDF loaded via assets</h1>
       <PDFViewer file={lorem} />
     </section>
   );
